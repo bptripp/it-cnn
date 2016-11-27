297a4a371a07f8728d367940fe21b35425802238
Bryan <bptripp@uwaterloo.ca>
2016-11-27 14:12:58 -0500

    adding Tanaka simplification code

---
+++ tuning/simplification.py
@@ -0,0 +1,113 @@
+__author__ = 'bptripp'
+
+import numpy as np
+import matplotlib
+matplotlib.rcParams['xtick.labelsize'] = 14
+matplotlib.rcParams['ytick.labelsize'] = 14
+import matplotlib.pyplot as plt
+from cnn_stimuli import get_image_file_list
+from alexnet import load_vgg, load_net, preprocess
+
+# remove_level = 2
+# use_vgg = False
+#
+# if use_vgg:
+#     model = load_vgg(weights_path='../weights/vgg16_weights.h5', remove_level=remove_level)
+# else:
+#     model = load_net(weights_path='../weights/alexnet_weights.h5', remove_level=remove_level)
+#
+#
+# from_dir = './images/simplification/from/'
+# from_image_files = get_image_file_list(from_dir, 'png', with_path=True)
+# from_out = model.predict(preprocess(from_image_files, use_vgg=use_vgg))
+#
+# to_dir = './images/simplification/to/'
+# to_image_files = get_image_file_list(to_dir, 'png', with_path=True)
+# to_out = model.predict(preprocess(to_image_files, use_vgg=use_vgg))
+#
+# #TODO: get null distribution by shuffling froms instead of using tos
+#
+# # baseline = np.min(np.concatenate((from_out, to_out), axis=0), axis=0)
+# baseline = np.min(from_out, axis=0)
+# from_out = from_out - baseline
+# to_out = to_out - baseline
+#
+# n = 50
+# n_shapes = from_out.shape[0]
+# # ratios = []
+# # nulls = []
+# plt.figure(figsize=(6,6))
+# # edges = np.linspace(0, 2.8, 15)
+# edges = np.linspace(0, 2.0, 11)
+# labels = ['plant', 'hind leg', 'cube', 'person', 'ball', 'haystack', 'pom-pom', 'apple', 'hand', 'skewer', 'bread', 'cat']
+# position = 1
+# ratio_fractions_over_one = np.zeros(n_shapes)
+# null_fractions_over_one = np.zeros(n_shapes)
+# for i in range(n_shapes):
+#     # response = from_out[i,:]
+#     ind = (-from_out[i,:]).argsort()[:n]
+#     ratios = to_out[i,ind] / from_out[i,ind]
+#     nulls = []
+#     for j in range(1,n_shapes):
+#         nulls.append(to_out[np.mod(i+j, n_shapes),ind] / from_out[i,ind])
+#     nulls = np.array(nulls)
+#
+#     #extra fussing to make order of panels like Tanaka
+#     plt.subplot(4,3,position)
+#     position = position + 3
+#     if position > 12:
+#         position = position - 11
+#
+#     ratio_counts, e = np.histogram(ratios.flatten(), edges)
+#     ratio_fractions = ratio_counts.astype(float) / np.sum(ratio_counts)
+#     null_counts, e = np.histogram(nulls.flatten(), edges)
+#     null_fractions = null_counts.astype(float) / np.sum(null_counts)
+#
+#     # plt.bar(edges[:-1]+.02, null_fractions, fill=False, width=.07)
+#     # plt.bar(edges[:-1]+.1, ratio_fractions, color=[1,0,0], width=.07)
+#     # plt.bar(edges[:-1], ratio_fractions - null_fractions, width=.2)
+#     plt.bar(edges[:-1], ratio_fractions, width=.2, color=[0,0,1])
+#     plt.bar(edges[:-1], -null_fractions, width=.2, color=[0.7,0.7,0.7])
+#     plt.xlim((0,2))
+#     # plt.ylim((-.8,.2))
+#     plt.ylim((-.8,.8))
+#     plt.xticks([0,1,2])
+#     plt.yticks([-.8,-.4,0,.4,.8])
+#     plt.title(labels[i])
+#     # plt.yticks([0,.5,1])
+#     # plt.yticks([])
+#     ratio_fractions_over_one[i] = float(np.sum(ratios.flatten() > 1)) / len(ratios.flatten())
+#     null_fractions_over_one[i] = float(np.sum(nulls.flatten() > 1)) / len(nulls.flatten())
+#
+# plt.tight_layout()
+#
+# print('>1 ' + str(np.mean(ratio_fractions_over_one)) + ' ' + str(np.mean(null_fractions_over_one)))
+#
+# if use_vgg:
+#     plt.savefig('../figures/simplification-vgg-' + str(remove_level) + '.eps')
+# else:
+#     plt.savefig('../figures/simplification-alexnet-' + str(remove_level) + '.eps')
+#
+# plt.show()
+
+# plot fractions over one (from repeated runs)
+remove_level = np.array([0,1,2])
+vgg_ratio_over_one = [0.146666666667, 0.123333333333, 0.115]
+vgg_null_over_one = [0.0984848484848, 0.0943939393939, 0.0916666666667]
+alexnet_ratio_over_one = [0.166666666667, 0.116666666667, 0.106666666667]
+alexnet_null_over_one = [0.0763636363636, 0.050303030303, 0.0604545454545]
+plt.figure(figsize=(6,2))
+plt.subplot(1,2,1)
+plt.plot(-remove_level, vgg_ratio_over_one, color='b')
+plt.plot(-remove_level, vgg_null_over_one, color='k')
+plt.ylim([0,.2])
+plt.title('VGG-16')
+plt.subplot(1,2,2)
+plt.plot(-remove_level, alexnet_ratio_over_one, color='b')
+plt.plot(-remove_level, alexnet_null_over_one, color='k')
+plt.ylim([0,.2])
+plt.title('Alexnet')
+# plt.xlabel('Distance from output (layers)')
+plt.tight_layout()
+plt.savefig('../figures/simplification-over-one.eps')
+plt.show()
